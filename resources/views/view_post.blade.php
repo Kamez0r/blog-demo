c81665d3d599e5ae27d876396e59b9adc628abd2
stefan <stefan@local.test>
2023-04-03 01:01:19 +0200

    modified the view that displays a post: added links on author and category to redirect to their coresponding index of posts

--- resources/views/view_post.blade.php
+++ resources/views/view_post.blade.php
@@ -3,12 +3,25 @@
 
 {{--@dd($publish_date)--}}
 
-<i>by {{$author['name']}}</i>
+<i>by
+    <a href="{{ URL::to("/author/" . $author['id']) }}">
+        {{$author['name']}}
+    </a>
+</i>
 
 <span> / </span>
 
 <i>{{ Carbon\Carbon::parse($publish_date)->format("Y-m-d H:i:s") }}</i>
 
+<span> / </span>
+
+
+
+<span> Category:
+    <a href="{{URL::to("/category/" . $category['id'])}}">
+        {{$category['title']}}
+    </a>
+</span>
 
 
 @foreach(explode("\n", $body) as $paragraph)
